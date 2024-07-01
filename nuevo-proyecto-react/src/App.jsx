9c2fa501b2555b26e27331b5442e02214817c073
Daniel Jaimes <danielDAJJ@outlook.com>
2024-06-30 19:25:58 -0500

    Se realiza la implementación del contexto, cerando un contexto global para el carrito, utilizando la función de agragar y quitar items de un carrito

--- nuevo-proyecto-react/src/App.jsx
+++ nuevo-proyecto-react/src/App.jsx
@@ -1,8 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import MainRouter from './routes/MainRouter';
+import { CartProvider } from './context/CartContext';
 
 function App() {
-  return <MainRouter/>
+  return (
+    <CartProvider>
+      <MainRouter/>
+    </CartProvider>
+  ) 
 }
 
 export default App
--- nuevo-proyecto-react/src/components/CartWidgetComponent/CartWidgetComponent.jsx
+++ nuevo-proyecto-react/src/components/CartWidgetComponent/CartWidgetComponent.jsx
@@ -1,12 +1,19 @@
+import React from 'react';
+
+import { CartContext } from '../../context/CartContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
+
 const CartWidgetComponent = () => {
     const customStyles = {fontSize: "1.25rem"}
+    const {cart} = React.useContext(CartContext);
+    const totalItems = cart.reduce((total, items) => total + items.quantity, 0);
+
   return (
     <div>
         <FontAwesomeIcon icon={faCartPlus} style={customStyles}/>
-        <span style={customStyles}>0</span>
+        <span style={customStyles}>{totalItems}</span>
     </div>
   )
 }
